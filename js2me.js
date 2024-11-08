50490a62df384ab5218755ed02576340bdc8d4b3
terrazah <94685370+terrazah@users.noreply.github.com>
2024-11-08 03:39:24 -0600

    Add files via upload

---
+++ js2me.js
@@ -0,0 +1,15 @@
+var js2me = {
+	// obsolete
+	JAVA_ROOT: 'javaRoot',
+	debug: false,
+	stat: 0,
+	calledMethods: {},
+	profile: false,
+	storageName: '',
+	config: {
+		app: true,
+		width: 240,
+		height: 266,
+		fullHeight: 320
+	}
+};
